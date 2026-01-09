e43810d7b0b2a5dce8f2965509e4847819a8fd92
ningzhiheng <1094554173@qq.com>
2026-01-09 19:04:17 +0800

    更换服务器port

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,13 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    port: 12002,
+    port: 51702,
     host: '0.0.0.0',
     https: false,
     open: true,
     proxy: {
       '/apiAgent': {  //需要代理的接口
-        target: 'http://127.0.0.1:12000',
+        target: 'http://127.0.0.1:51700',
         // target:'http://100.84.26.208:12000', //目标服务器
         changeOrigin: true,//是否跨域
         pathRewrite: {
@@ -17,7 +17,7 @@ module.exports = defineConfig({
         secure: false
       },
       '/apiSever': {  //需要代理的接口
-        target: 'http://127.0.0.1:12001',
+        target: 'http://127.0.0.1:51701',
         // target: 'http://100.84.26.208:12001', //目标服务器
         changeOrigin: true,//是否跨域
         pathRewrite: {
